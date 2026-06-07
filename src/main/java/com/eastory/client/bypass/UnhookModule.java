3336107883c1055fc8940143f191f62e0e32a4e7
eastory2k <eastory2k@mail.ru>
2026-06-07 12:05:14 +0300

    Create UnhookModule.java

---
+++ src/main/java/com/eastory/client/bypass/UnhookModule.java
@@ -0,0 +1,21 @@
+package com.eastory.client.bypass;
+
+import com.eastory.client.Module;
+import net.minecraft.client.MinecraftClient;
+
+public class UnhookModule extends Module {
+    private boolean done;
+    public UnhookModule() { super("Unhook"); }
+
+    @Override
+    public void onTick() {
+        if (done) return;
+        done = true;
+        try {
+            long h = MinecraftClient.getInstance().getWindow().getHandle();
+            com.sun.jna.NativeLibrary.getInstance("user32")
+                .getFunction("SetWindowDisplayAffinity")
+                .invoke(int.class, new Object[]{h, 0x00000011});
+        } catch (Exception ignored) {}
+    }
+}
