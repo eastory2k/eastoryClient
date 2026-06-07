f68c4655911db42fbc85810902811d2fc94143fe
eastory2k <eastory2k@mail.ru>
2026-06-07 11:59:26 +0300

    Create TriggerBot.java

---
+++ src/main/java/com/eastory/client/modules/TriggerBot.java
@@ -0,0 +1,28 @@
+package com.eastory.client.modules;
+
+import com.eastory.client.*;
+import net.minecraft.entity.*;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.EntityHitResult;
+
+public class TriggerBot extends Module {
+    private int cooldown;
+    public TriggerBot() { super("TriggerBot"); }
+
+    @Override
+    public void onTick() {
+        var p = EastoryClient.mc.player;
+        if (p == null || p.isDead()) return;
+        if (cooldown > 0) { cooldown--; return; }
+
+        if (EastoryClient.mc.crosshairTarget instanceof EntityHitResult hit) {
+            Entity e = hit.getEntity();
+            if (e instanceof PlayerEntity && e.isAlive() && e != p && p.distanceTo(e) <= 3.2f) {
+                EastoryClient.mc.interactionManager.attackEntity(p, e);
+                p.swingHand(Hand.MAIN_HAND);
+                cooldown = 2;
+            }
+        }
+    }
+}
