584607f20b1519b7e1ee349ae49110a1c64234a4
eastory2k <eastory2k@mail.ru>
2026-06-07 23:06:23 +0300

    Update ClientModule.java

--- src/main/java/com/eastory/client/ClientModule.java
+++ src/main/java/com/eastory/client/ClientModule.java
@@ -1,8 +1,11 @@
 package com.eastory.client;
 
+import net.minecraft.client.option.KeyBinding;
+
 public class ClientModule {
     public String name;
     public boolean enabled = true;
+    public KeyBinding keyBinding;
 
     public ClientModule(String name) { this.name = name; }
     public void onTick() {}
